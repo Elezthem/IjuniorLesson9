737c519957e321e5b767112899938ae3faf7d4fc
HemyDev <97851451+Elezthem@users.noreply.github.com>
2023-08-26 09:42:38 +0300

    Add files via upload

---
+++ Ijunior/Program.cs
@@ -0,0 +1,31 @@
+﻿int magicNumber = 4;
+
+Console.WriteLine("Угадай число");
+Console.WriteLine();
+Console.WriteLine("Загадано число от 1 до 5. Попробуйте его отгадать!");
+Console.WriteLine();
+Console.Write("Введите ваше число: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+//
+
+if (input == 1)
+{
+    Console.WriteLine("Нет! Это не " + input + "!");
+}
+else if (input == 2)
+{
+    Console.WriteLine("Нет! Это не " + input + "!");
+}
+else if (input == 3)
+{
+    Console.WriteLine("Нет! Это не " + input + "!");
+}
+else if (input == 5)
+{
+    Console.WriteLine("Нет! Это не " + input + "!");
+}
+else if (input == 4)
+{
+    Console.WriteLine("Да! Это число " + magicNumber + "!");
+}
